e067b665dd9b40e17c59378f041f72546fa97462
David Simmons <davidsimmons@Davids-MacBook-Pro.local>
2022-02-13 09:03:48 +0000

    Store Blog Posts as HTML files.

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sint tempora distinctio laborum
+            dolor
+            quia consectetur necessitatibus. Dolores sed non cum laudantium, aliquid sequi, nobis explicabo
+            dignissimos
+            odit earum doloribus?</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sint tempora distinctio laborum dolor
+            quia consectetur necessitatibus. Dolores sed non cum laudantium, aliquid sequi, nobis explicabo dignissimos
+            odit earum doloribus?</p>
+    </article>
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sint tempora distinctio laborum dolor
+            quia consectetur necessitatibus. Dolores sed non cum laudantium, aliquid sequi, nobis explicabo dignissimos
+            odit earum doloribus?</p>
+    </article>
+</body>
+
+</html>
