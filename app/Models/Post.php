a498c60024ce661e531f56b5d9f3ab62e0db86b4
Dave Simmons <shortlyportly@googlemail.com>
2022-02-15 19:45:20 +0000

    Find a Composer package for metadata

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,26 +1,41 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    public $title;
+    public $slug;
+    public $excerpt;
+    public $date;
+    public $body;
+
+    public function __construct($title, $slug, $excerpt, $date, $body)
+    {
+        $this->title = $title;
+        $this->slug = $slug;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+    }
+
     public static function all()
     {
-        $files = File::files(resource_path('posts/'));
-
-        return array_map(function ($file) {
-            return $file->getContents();
-        }, $files);
+        return collect(File::files(resource_path('posts')))
+            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn ($document) => new Post(
+                $document->title,
+                $document->slug,
+                $document->excerpt,
+                $document->date,
+                $document->body()
+            ));
     }
 
     public static function find($slug)
     {
-        if (!file_exists($path = resource_path("/posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember('posts.{slug}', 5, fn () => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 }
