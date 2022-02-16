31ecca559ae355d471ce3382cefc4ccd493538e9
Dave Simmons <shortlyportly@googlemail.com>
2022-02-16 17:22:10 +0000

    Option 1 - using Blade Directives (section)

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-
-<body>
+    @extends('layout')
+    @section('content')
     <article>
         <h1>{{ $post->title }}</h1>
         <div>
@@ -17,6 +7,4 @@
         </div>
     </article>
     <a href="/">Go Back</a>
-</body>
-
-</html>
+    @endsection
