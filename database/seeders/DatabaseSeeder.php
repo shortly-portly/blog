eaf52ed0d84426e3d3a3600abe64c62ea198f50f
Dave Simmons <shortlyportly@googlemail.com>
2022-02-19 17:52:10 +0000

    Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 namespace Database\Seeders;
 
-use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -14,25 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Clear out the relevant tables
-        User::truncate();
-        Category::truncate();
-
-        $user = User::factory(10)->create();
-
-        Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'Dave Simmons'
         ]);
 
-        Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
+        Post::factory(10)->create([
+            'user_id' => $user->id
         ]);
     }
 }
