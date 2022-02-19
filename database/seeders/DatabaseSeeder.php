0eaaa95583261a2722371d37844276ab85d6e229
Dave Simmons <shortlyportly@googlemail.com>
2022-02-19 06:58:40 +0000

    Introduction to seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
-
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Clear out the relevant tables
+        User::truncate();
+        Category::truncate();
+
+        $user = User::factory(10)->create();
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
     }
 }
