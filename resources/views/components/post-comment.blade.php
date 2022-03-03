2a3c7648b43b505022284372e8a7225ec0e79ace
Dave Simmons <shortlyportly@googlemail.com>
2022-03-03 23:06:38 +0000

    Add comments form

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -2,12 +2,12 @@
 
 <article class="flex bg-gray-100 border border-gray-200 rounded-xl p-6 space-x-4">
   <div class="flex-shrink-0">
-    <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
+    <img src="https://i.pravatar.cc/60?u={{ $comment->user_id }}" alt="" width="60" height="60" class="rounded-xl">
   </div>
   <div>
     <header class="mb-4">
       <h3 class="font-bold">{{ $comment->author->username }}</h3>
-      <p class="text-xs">Posted <time>{{ $comment->created_at }}</time></p>
+      <p class="text-xs">Posted <time>{{ $comment->created_at->format('F j, Y, g:i a') }}</time></p>
     </header>
     <p>
       {{ $comment->body }}
