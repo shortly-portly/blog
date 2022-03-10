5d7d1b21f60897148125960f17e6d665b233d864
Dave Simmons <shortlyportly@googlemail.com>
2022-03-10 21:55:29 +0000

    Added email and mailchimp

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
+Route::post('newsletter', NewsletterController::class);
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
@@ -25,7 +27,7 @@ Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::get('login', [SessionController::class, 'create'])->middleware('guest');
+Route::get('login', [SessionController::class, 'create'])->middleware('guest')->name('login');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
 
